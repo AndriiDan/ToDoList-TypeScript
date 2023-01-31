20c7eb569c8becce52e3bdfb1e43041755a21fdc
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2023-01-31 13:05:46 +0200

    Add useState for tasks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { TaskType, Todolist } from './Todolist';
 
-// можна експортувати TaskType з Todolst.tsx, щоб tasks відповідав масиву об'єктів TaskType - мінімізує імовірність помилки при формуванні об'єктів
-// let tasks: TaskType[] = [
-let tasks: Array<TaskType> = [
-  { id: 1, title: "CSS&HTML", isDone: true },
-  { id: 2, title: "JavaScript", isDone: true },
-  { id: 3, title: "React", isDone: false },
-  { id: 4, title: "Redux", isDone: false }
-]
-
-// ф-ція для видалення task
-function removeTask(id: number) {
-  tasks = tasks.filter(t => t.id !== id); // При "t.id !== id" (=true) проходить фільтрацію
-  console.log(tasks);
-}
-
 function App() {
+  // можна експортувати TaskType з Todolst.tsx, щоб tasks відповідав масиву об'єктів TaskType - мінімізує імовірність помилки при формуванні об'єктів
+  // let tasks: TaskType[] = [
+  let initTasks: Array<TaskType> = [
+    { id: 1, title: "CSS&HTML", isDone: true },
+    { id: 2, title: "JavaScript", isDone: true },
+    { id: 3, title: "React", isDone: false },
+    { id: 4, title: "Redux", isDone: false }
+  ]
+
+  // Локальний state для tasks
+  let [tasks, setTasks] = useState(initTasks);
+
+  // ф-ція для видалення task
+  function removeTask(id: number) {
+    let filteredTasks = tasks.filter(t => t.id !== id); // При "t.id !== id" (=true) проходить фільтрацію
+    setTasks(filteredTasks);
+  }
+
   return (
     <div className="App">
       <Todolist title="What to learn" tasks={tasks} removeTask={removeTask} />
